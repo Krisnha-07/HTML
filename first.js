202007cdb85c8d9e9417634856c221d013803754
Krisnha-07 <krishnagope2005@gmail.com>
2025-07-08 21:38:38 +0530

    JS
    day 1 of Javascript

--- first.js
+++ first.js
@@ -1,1 +1,29 @@
-console.log("hello world how r u");
+//Date - 08/07/25
+
+/*let num = prompt("enter a no");
+if (num%5 == 0){
+    console.log(num,"num is Multiple of 5")
+}
+else{console.log(num,"num is not Multiple of 5")} 
+
+let score = 999;
+let grade;
+if (score>=100){
+    grade="A+";
+}
+else if (score>=90 && score <=100) {
+    grade ="A";
+}
+    else if(score>=60 && score<=69) {
+        grade = "C";
+    }
+    else if(score>=50 && score<=59) {
+        grade ="D";
+    }
+     else if(score>=0 && score<=49) {
+        grade = "F";}
+console.log("Your Grade",grade)  */
+
+
+
+//Date - 08/07/25
